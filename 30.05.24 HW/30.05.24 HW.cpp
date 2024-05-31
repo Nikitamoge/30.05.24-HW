980be42100bfb3e35d520fc23dc114346965f1a0
Nikita Iliuschchenko <Nike.il.od@gmail.com>
2024-05-31 15:35:12 +0200

    Add project files.

---
+++ 30.05.24 HW/30.05.24 HW.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class RotatingDrum
+{
+public:
+    void Rotate()
+    {
+        cout << "The rotating drum is spinning" << endl;
+    }
+};
+
+class MachineEngine
+{
+public:
+    void Initiate()
+    {
+        cout << "The machine engine is initiating" << endl;
+    }
+    void Terminate()
+    {
+        cout << "The machine engine is terminating" << endl;
+    }
+};
+
+class WaterSupplySystem
+{
+public:
+    void SupplyWater()
+    {
+        cout << "The water supply system is filling water" << endl;
+    }
+};
+
+class WaterDrainageSystem
+{
+public:
+    void DrainWater()
+    {
+        cout << "The water drainage system is draining water" << endl;
+    }
+};
+
+class WashingMachineFacade
+{
+    RotatingDrum rotatingDrum;
+    MachineEngine machineEngine;
+    WaterSupplySystem waterSupplySystem;
+    WaterDrainageSystem waterDrainageSystem;
+public:
+    WashingMachineFacade(RotatingDrum rotatingDrum, MachineEngine machineEngine, WaterSupplySystem waterSupplySystem, WaterDrainageSystem waterDrainageSystem)
+    {
+        this->rotatingDrum = rotatingDrum;
+        this->machineEngine = machineEngine;
+        this->waterSupplySystem = waterSupplySystem;
+        this->waterDrainageSystem = waterDrainageSystem;
+    }
+    void StartWashing()
+    {
+        machineEngine.Initiate();
+        rotatingDrum.Rotate();
+        waterSupplySystem.SupplyWater();
+    }
+    void StopWashing()
+    {
+        waterDrainageSystem.DrainWater();
+        machineEngine.Terminate();
+    }
+};
+
+class User
+{
+public:
+    void UseWashingMachine(WashingMachineFacade washingMachineFacade)
+    {
+        washingMachineFacade.StartWashing();
+        washingMachineFacade.StopWashing();
+    }
+};
+
+int main()
+{
+    RotatingDrum rotatingDrum;
+    MachineEngine machineEngine;
+    WaterSupplySystem waterSupplySystem;
+    WaterDrainageSystem waterDrainageSystem;
+
+    WashingMachineFacade washingMachineFacade(rotatingDrum, machineEngine, waterSupplySystem, waterDrainageSystem);
+
+    User user;
+    user.UseWashingMachine(washingMachineFacade);
+}
